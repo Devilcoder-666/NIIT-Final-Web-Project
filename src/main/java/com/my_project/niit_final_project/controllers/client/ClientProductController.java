fae4b85c39c6b7e61d0a9c13b738dfe23224f66e
Devilcoder-666 <zexaldai@gmail.com>
2021-10-06 23:00:03 +0700

    Fixing bill ajax(ok) Fixing pages(ok) add page product by type(ok)

--- src/main/java/com/my_project/niit_final_project/controllers/client/ClientProductController.java
+++ src/main/java/com/my_project/niit_final_project/controllers/client/ClientProductController.java
@@ -14,6 +14,11 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
 @Controller
 @RequestMapping("/client")
 public class ClientProductController {
@@ -37,12 +42,11 @@ public class ClientProductController {
             model.addAttribute("banner_1", banner_1);
             model.addAttribute("banner_2", banner_2);
             model.addAttribute("banner_3", banner_3);
-
             }
             Page<Product> listNewProduct = productService.getPageNewProduct(0, 8);
             Page<Product> listSaleProduct = productService.getPageSaleProduct(0, 8);
             Page<ProductType> listProductType = productTypeService.getPageProductType(0, 6);
-            ProductType productType = new ProductType();
+
             int u = productTypeService.getProductTypeById(4).getCategories().size();
             model.addAttribute("listNewProduct", listNewProduct.iterator());
             model.addAttribute("listSaleProduct", listSaleProduct.iterator());
@@ -70,6 +74,9 @@ public class ClientProductController {
 
     }
 
+
+
+
     @GetMapping("/category/product")
     private String getCategoryProduct(Model model, @RequestParam(name = "id") long id, @RequestParam(name = "page", defaultValue = "0") int page, @RequestParam(name = "activePage", defaultValue = "0") int activePage) {
 
@@ -78,6 +85,7 @@ public class ClientProductController {
             if (page > totalPage + 1) {
                 Page<Product> listProductByCategory = productService.getPageProductByCategoryId(activePage, 10, id);
                 Page<ProductType> listProductType = productTypeService.getPageProductType(0, 6);
+
                 model.addAttribute("listProductType", listProductType.iterator());
                 model.addAttribute("listProductByCategory", listProductByCategory);
                 model.addAttribute("activePage", activePage);
@@ -103,6 +111,33 @@ public class ClientProductController {
 
 
 
+    }
+    @GetMapping("/type/product")
+    private String getTypeProduct(Model model, @RequestParam(name = "id") long id, @RequestParam(name = "page", defaultValue = "0") int page, @RequestParam(name = "activePage", defaultValue = "0") int activePage) {
+
+        try {
+
+            int totalPage =productTypeService.getProductsByTypeId( 10, id).size();
+            //paging logic
+            if (page > totalPage + 1) {
+                List<Product> listProductByType = productTypeService.getProductsByTypeId(10,id).get(activePage);
+                model.addAttribute("listProductByType", listProductByType);//this is list of product for show
+                model.addAttribute("activePage", activePage);
+            } else {
+                List<Product> listProductByType = productTypeService.getProductsByTypeId(10,id).get(page);
+                model.addAttribute("listProductByType", listProductByType);//this is list of product for show
+                model.addAttribute("activePage", page);
+            }} catch (Exception e){
+            System.out.println(e);
+        }
+        finally{
+            Page<ProductType> listProductType = productTypeService.getPageProductType(0, 6);
+            model.addAttribute("listProductType", listProductType.iterator());//this is list product for navigate
+            ProductType productType = productTypeService.getProductTypeById(id);
+            model.addAttribute("navType",productType);
+            model.addAttribute("id", id);
+            model.addAttribute("totalPage",productTypeService.getProductsByTypeId( 10, id).size() );
+            return "/client/classify/type";}
     }
 
     @GetMapping("/search")
--- src/main/java/com/my_project/niit_final_project/services/ProductTypeService.java
+++ src/main/java/com/my_project/niit_final_project/services/ProductTypeService.java
@@ -1,5 +1,7 @@
 package com.my_project.niit_final_project.services;
 
+import com.my_project.niit_final_project.entities.Category;
+import com.my_project.niit_final_project.entities.Product;
 import com.my_project.niit_final_project.entities.ProductType;
 
 import com.my_project.niit_final_project.repositories.ProductTypeRepository;
@@ -11,12 +13,43 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.*;
 
 @Service
 public class ProductTypeService {
 
     @Autowired
     ProductTypeRepository productTypeRepository;
+
+    public  <T> List<List<T>> getPages(Collection<T> c, Integer pageSize) {
+        if (c == null)
+            return Collections.emptyList();
+        List<T> list = new ArrayList<T>(c);
+        if (pageSize == null || pageSize <= 0 || pageSize > list.size())
+            pageSize = list.size();
+        int numPages = (int) Math.ceil((double)list.size() / (double)pageSize);
+        List<List<T>> pages = new ArrayList<List<T>>(numPages);
+        for (int pageNum = 0; pageNum < numPages;)
+            pages.add(list.subList(pageNum * pageSize, Math.min(++pageNum * pageSize, list.size())));
+        return pages;
+    }
+
+
+    public  List<List<Product>> getProductsByTypeId(int size,long id){
+        ProductType productType=getProductTypeById(id);
+        Collection<Category> categories=   productType.getCategories();
+        Collection<Product> products=new HashSet<Product>();
+          for (Category category:categories){
+              for(Product product: category.getProducts()){
+                  products.add(product);
+              }
+          }
+        List<List<Product>> listPageProduct= getPages(products,size);
+        return listPageProduct;
+
+    }
+
+
     public  Long countTotal(){
         return productTypeRepository.count();
     }
